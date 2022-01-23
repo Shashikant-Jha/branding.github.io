34cca8319329f21f04472088b128acada67a61df
Shashikant Jha <jhashashi669@outlook.com>
2022-01-23 11:23:43 +0530

    analytics setup

---
+++ js/messenger.js
@@ -0,0 +1,8 @@
+const dfMessenger = document.querySelector('df-messenger');
+dfMessenger.addEventListener('df-messenger-loaded', function (event) {
+  // Handle event
+  console.log('DF Messenger loaded')
+//   dfMessenger.addEventListener('df-response-received', (respEvt) => {
+//     console.log(respEvt)
+//   });
+});
